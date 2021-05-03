7729a0a51e15edde3d18cb4e19bff540c82f6bfa
Nate <n8blake@mac.com>
2021-05-02 20:57:29 -0700

    Add tag and product_tag definitions

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,24 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: 'product', 
+        key: 'product_id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: 'tag', 
+        key: 'tag_id'
+      }
+    },
   },
   {
     sequelize,
