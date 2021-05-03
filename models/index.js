e7fb3a8c0e768e6e12e46e017c0e2268d47dac7c
Nate <n8blake@mac.com>
2021-05-02 21:21:41 -0700

    Add Category GET routes

--- models/index.js
+++ models/index.js
@@ -5,9 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-	foreignKey: 'category_id'
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
 Category.hasMany(Product);
@@ -18,6 +16,9 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: 'ProductTag'
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,22 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Category.findAll().catch((err) => {
+  	res.json(err);
+  });
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const category = await Category.findByPk(req.params.id).catch((err) => {
+  	res.json(err);
+  });
+  res.json(category);
 });
 
 router.post('/', (req, res) => {
