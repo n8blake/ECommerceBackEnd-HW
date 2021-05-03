e8b6c16b0da4e235933b9a527b2359271ce20994
Nate <n8blake@mac.com>
2021-05-03 09:29:45 -0700

    Add includes in Products and Categories GET routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  const categories = await Category.findAll().catch((err) => {
+  const categories = await Category.findAll({
+			include: {
+				model: Product
+			}
+		}).catch((err) => {
   	res.json(err);
   });
   res.json(categories);
@@ -15,7 +19,9 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  const category = await Category.findByPk(req.params.id).catch((err) => {
+  const category = await Category.findByPk(req.params.id, {
+  	include:[{model: Product}]
+  }).catch((err) => {
   	res.json(err);
   });
   res.json(category);
