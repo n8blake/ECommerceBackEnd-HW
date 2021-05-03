8736daf1036f5358f0f22db1257ba3ad36453b49
Nate <n8blake@mac.com>
2021-05-03 10:51:13 -0700

    Add POST, PUT and DELETE methods to Tag Routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,14 +29,43 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  /* req.body should look like this...
+    {
+      tag_name: "sports",
+    }
+  */
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+   	.catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {where: {tag_id: req.params.id}})
+  	.then((code) => {
+  		code == 1 ? res.status(200).json(code) : res.status(400).json(code);   
+  	})
+  	.catch((err) => {
+  		console.log(err);
+  		res.status(400).json(err);
+  	});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({where: {tag_id: req.params.id}})
+  	.then((code) => {
+  		code == 1 ? res.status(200).json(code) : res.status(400).json(code); 
+  	})
+  	.catch((err) => {
+  		console.log(err);
+  		res.status(400).json(err);
+  	});
 });
 
 module.exports = router;
